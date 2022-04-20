089cf804099e4ddb8e8741226157f04d6eb8412b
DEYIWANG <49895510+deyiwang89@users.noreply.github.com>
2022-04-20 09:30:36 +0800

    Update summary.py
    调整了summary，可以正常运行

--- summary.py
+++ summary.py
@@ -6,5 +6,5 @@ from nets.yolo4_tiny import YoloBody
 if __name__ == "__main__":
     # 需要使用device来指定网络在GPU还是CPU运行
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    m       = YoloBody([[3, 4, 5], [1, 2, 3]], 2).to(device)
-    summary(m, input_size=(3, 416, 416))
+    m       = YoloBody(2).to(device)
+    summary(m, input_data=(3, 416, 416))
